4b11c0428a6d835311569847c186e9b7303ba8b2
sugo1629 <spaulding.cody@gmail.com>
2022-12-07 12:19:32 -0800

    completed day 7

--- day 6/day6.py
+++ day 6/day6.py
@@ -1,21 +1,23 @@
 import sys
 
-# Part 1
-with open(f'{sys.path[0]}/data.txt') as f:
-    data = [x for x in f.read().splitlines()]
-
 
 def tuner(unique_count, input_data):
     chars = set()
     for i, char in enumerate(input_data[0]):
         chars.add(char)
         for c in range(i + 1, i + unique_count):
-            chars.add(input_data[0][c])
+            try:
+                chars.add(input_data[0][c])
+            except IndexError:
+                return 0
         if len(chars) == unique_count:
             return i + unique_count
         chars = set()
     return 0
 
+
+with open(f'{sys.path[0]}/data.txt') as f:
+    data = [x for x in f.read().splitlines()]
 
 print(f'Part 1: {tuner(4, data)}')
 print(f'Part 2: {tuner(14, data)}')
--- day 7/day7.py
+++ day 7/day7.py
@@ -1,7 +1,45 @@
+from string import digits
+from collections import defaultdict
 import sys
 
-# Part 1
-with open(f'{sys.path[0]}/data.txt') as f:
-    data = [x for x in f.read().splitlines()]
 
-# TODO: Puzzle
+def parse_data():
+    with open(f'{sys.path[0]}/data.txt') as f:
+        return f.read().splitlines()
+
+
+data = parse_data()
+dirs = defaultdict(int)
+
+current_path = '/'
+for i, line in enumerate(data):
+    if '$ cd ' in line and '$ cd /' not in line:
+        if '..' in line:
+            current_path = '/'.join(current_path.split('/')[0:-1])
+        else:
+            new_dir = line.split(' ')[2]
+            current_path = f'{current_path}/{new_dir}'
+    if line[0] in digits:
+        file_size = int(line.split(' ')[0])
+        dirs[current_path] += file_size
+        parents = current_path.split('/')
+        for parent in range(1, len(parents) + 1):
+            parent_path = '/'.join(current_path.split('/')[0:-parent])
+            dirs[parent_path] += file_size
+
+
+puzzle = 0
+total_root = dirs['/']
+for k, v in dirs.items():
+    if v <= 100000:
+        puzzle += v
+
+print(f'Part 1: {puzzle}')
+
+puzzle = []
+needed_space = 30_000_000 - (70_000_000 - total_root)
+for k, v in dirs.items():
+    if v >= needed_space:
+        puzzle.append(v)
+
+print(f'Part 2: {min(puzzle)}')
